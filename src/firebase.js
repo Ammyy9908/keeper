cf729234182959116b39b83f216f0612320ca2d2
sumit kumar bighaniya <sb78639@gmail.com>
2020-09-24 17:41:35 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,6 @@
 import firebase from 'firebase'
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyACy8Kg_skFAgF6cw-E5WAOfWmFNe4TncI",
-    authDomain: "keep-953d7.firebaseapp.com",
-    databaseURL: "https://keep-953d7.firebaseio.com",
-    projectId: "keep-953d7",
-    storageBucket: "keep-953d7.appspot.com",
-    messagingSenderId: "9173474545",
-    appId: "1:9173474545:web:22247f3d29baeb9fbb9d50",
-    measurementId: "G-PTLPB2GE9C"
-  };
+const firebaseConfig = /*Your Firebase Config Comes Here*/
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const auth = firebaseApp.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
